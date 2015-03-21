b3b0924e2d0078fef25743bfd22c23367108bbe2
Connor Kennedy <cjtkennedy@gmail.com>
2015-03-21 00:43:18 -0500

    Added some more personal scripts

---
+++ Create_sotrage_space_disk.ps1
@@ -0,0 +1,65 @@
+﻿#############################################################################
+#############################################################################
+####                                                                     ####
+####   Creates a new virtual disk in the 'SP1' storage pool using the    ####
+####                       specified parameters                          ####
+####                                                                     ####
+#############################################################################
+################################ Disk Info ##################################
+####                                                                     ####
+<##>  $diskName = "PlexStorage_drive1"                                   ####
+<##>  $diskSize = "1600gb"                                               ####
+<##>  $writeCacheSize = "16gb"                                           ####
+<##>  $resiliencySetting = "Simple"  # Simple, Mirror, or Parity         ####
+<##>  $provisioningType = "Fixed"    # Fixed, Thin, or Unknown           ####
+####                                                                     ####
+#############################################################################
+############################# Partition Info ################################
+####                                                                     ####
+<##>  $partitionStyle = "GPT"        # MBR or GPT                        ####
+<##>  $fileSystem = "ReFS"           # NTFS, ReFS, exFAT, FAT32, or FAT  ####
+<##>  $partitionName = "PlexStorageContainer"                            ####
+####                                                                     ####
+#############################################################################
+#############################################################################
+
+""
+"Creating $diskName..."
+
+# Create command
+$cmd = [String]::Format("New-VirtualDisk -StoragePoolFriendlyName `"SP1`" -FriendlyName `"$diskName`" -ResiliencySettingName $resiliencySetting -WriteCacheSize $writeCacheSize -ProvisioningType $provisioningType -Size $diskSize")
+
+# Run command to create virtual disk
+$cmdRes = iex $cmd
+
+"Disk `"$diskName`" was created successfully!"
+""
+"Formatting new disk..."
+[System.Threading.Thread]::Sleep(500)
+
+# Get newly created disk
+$newDisk = Get-Disk | Where PartitionStyle -eq RAW | Where Size -eq $cmdRes.Size
+
+# Initialize and format newly created disk
+$newVolume = $newDisk | Initialize-Disk -PartitionStyle $partitionStyle -PassThru | 
+New-Partition -AssignDriveLetter -UseMaximumSize | 
+Format-Volume -FileSystem $fileSystem -NewFileSystemLabel $partitionName -Confirm:$false
+
+# Get user-friendly size
+$labels = @("B", "KB", "MB", "GB", "TB")
+$runs = 0
+$size = $newVolume.SizeRemaining
+$label = ""
+while (1){
+    if ($size / 1024 -lt 1){
+        $label = [String]::Format("{0} {1}", $size, $labels[$runs])
+        break
+    }
+    else{
+        $runs++
+        $size = $size / 1024
+    }
+}
+
+## Display final output
+[String]::Format("The {0} partiton `"{1}`" was successfully created and mounted at {2}: with {3} of free space", $newVolume.FileSystem, $newVolume.FileSystemLabel, $newVolume.DriveLetter, $label)
--- mount_network_drives.ps1
+++ mount_network_drives.ps1
@@ -1,11 +1,10 @@
 ﻿$name = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name;
 if ($name.Contains('\')){
     $name = $name.Substring($name.LastIndexOf('\') + 1);
-    $name = $name.Chars(0).ToString().ToUpper() + $name.Substring(1)
 }
+$name = $name.Chars(0).ToString().ToUpper() + $name.Substring(1)
 $mappings = @{
-    "T" = "\\PLEX\Plex";
-    "U" = "\\PLEX\Plex2";
+    "U" = "`"\\PLEX\Plex`"";
     "V" = "`"\\TESLA\Backup`"";
     "W" = "`"\\TESLA\$name$`"";
     "X" = "`"\\TESLA\TWN Storage`"";
@@ -15,5 +14,5 @@ $mappings = @{
 $mappings = $mappings.GetEnumerator() | Sort-Object Name
 for ($i=0; $i -lt $mappings.Count; $i++){
     $cmd = [System.String]::Format("net use {0}: {1} /persistent:yes", $mappings[$i].Key, $mappings[$i].Value)
-    $cmd
+    iex $cmd
 }
