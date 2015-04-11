dd9a404d10644513a10a6a321618b236b9199513
Connor Kennedy <cjtkennedy@gmail.com>
2015-04-11 16:12:01 -0500

    New theme + non-tierred storage space deduplication

--- CreateNonTieredStorageSpaceDisk.ps1
+++ CreateNonTieredStorageSpaceDisk.ps1
@@ -1,24 +1,31 @@
 ﻿#############################################################################
 #############################################################################
 ####                                                                     ####
-####   Creates a new non-tiered virtual disk in the 'SP1' storage pool   ####
-####               using the parameters specified below                  ####
+####             Creates a new non-tiered virtual disk using             ####
+####                   the parameters specified below                    ####
 ####                                                                     ####
 #############################################################################
 ################################ Disk Info ##################################
 ####                                                                     ####
-<##>  $diskName = "PlexStorage_drive1"                                   ####
-<##>  $diskSize = "1600gb"                                               ####
-<##>  $writeCacheSize = "16gb"                                           ####
-<##>  $resiliencySetting = "Simple"  # Simple, Mirror, or Parity         ####
-<##>  $provisioningType = "Thin"    # Fixed, Thin, or Unknown            ####
+<##>  $storagePool = "Pool1"         # Pool in which to create disk      ####
+<##>  $diskName = "Disk1"            # Name of new disk                  ####
+<##>  $diskSize = "2048gb"           # Size of new disk                  ####
+<##>  $writeCacheSize = "8gb"        # Writeback cache size              ####
+<##>  $resiliencySetting = "Mirror"  # Simple, Mirror, or Parity         ####
+<##>  $provisioningType = "Thin"     # Fixed, Thin, or Unknown           ####
 ####                                                                     ####
 #############################################################################
 ############################# Partition Info ################################
 ####                                                                     ####
 <##>  $partitionStyle = "GPT"        # MBR or GPT                        ####
 <##>  $fileSystem = "ReFS"           # NTFS, ReFS, exFAT, FAT32, or FAT  ####
-<##>  $partitionName = "PlexStorageContainer"                            ####
+<##>  $partitionName = "Partition1"  # Name of new partition             ####
+####                                                                     ####
+#############################################################################
+########################### Deduplication Info ##############################
+####                                                                     ####
+<##>  $enableDeduplication = $true   # $true or $false                   ####
+<##>  $minimumFileAge = 1            # Days                              ####
 ####                                                                     ####
 #############################################################################
 #############################################################################
@@ -27,7 +34,7 @@
 "Creating $diskName..."
 
 # Create command
-$cmd = "New-VirtualDisk -StoragePoolFriendlyName `"SP1`" -FriendlyName `"$diskName`" -ResiliencySettingName $resiliencySetting -WriteCacheSize $writeCacheSize -ProvisioningType $provisioningType -Size $diskSize"
+$cmd = "New-VirtualDisk -StoragePoolFriendlyName `"$storagePool`" -FriendlyName `"$diskName`" -ResiliencySettingName $resiliencySetting -WriteCacheSize $writeCacheSize -ProvisioningType $provisioningType -Size $diskSize"
 
 # Run command to create virtual disk
 $cmdRes = iex $cmd
@@ -35,6 +42,10 @@ $cmdRes = iex $cmd
 "Disk `"$diskName`" was created successfully!"
 ""
 "Formatting new disk..."
+
+# Wait for new disk to mount after creation
+# This might need to be adjusted if the machine has a slow storage interface
+# There might be a better way to do this than to just wait half a second
 [System.Threading.Thread]::Sleep(500)
 
 # Get newly created disk
@@ -45,14 +56,26 @@ $newVolume = $newDisk | Initialize-Disk -PartitionStyle $partitionStyle -PassThr
 New-Partition -AssignDriveLetter -UseMaximumSize | 
 Format-Volume -FileSystem $fileSystem -NewFileSystemLabel $partitionName -Confirm:$false
 
+"Format completed successfully!"
+
+# Enable data deduplication (NTFS only)
+if ($newVolume.FileSystem.ToLower() -eq "ntfs"){
+    ""
+    "Enabling Data Deduplication..."
+    $dl = [String]::Format("{0}:", $newVolume.DriveLetter)
+    $r = Enable-DedupVolume $dl
+    Set-DedupVolume $dl -MinimumFileAgeDays $minimumFileAge
+    "Data Deduplication enabled successfully!"
+}
+
 # Get user-friendly size
-$labels = @("B", "KB", "MB", "GB", "TB")
+$labels = @("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
 $runs = 0
 $size = $newVolume.SizeRemaining
 $label = ""
 while (1){
     if ($size / 1024 -lt 1){
-        $label = [String]::Format("{0} {1}", $size, $labels[$runs])
+        $label = [String]::Format("{0:0.##} {1}", $size, $labels[$runs])
         break
     }
     else{
@@ -62,4 +85,15 @@ while (1){
 }
 
 ## Display final output
-[String]::Format("The {0} partiton `"{1}`" was successfully created and mounted at {2}: with {3} of free space", $newVolume.FileSystem, $newVolume.FileSystemLabel, $newVolume.DriveLetter, $label)
+$fo = [String]::Format("The {0} partiton `"{1}`" was created successfully and mounted at {2}: with {3} of free space.{4}", $newVolume.FileSystem, $newVolume.FileSystemLabel, $newVolume.DriveLetter, $label, [Environment]::NewLine)
+if ($enableDeduplication -eq $true){
+    $fo += "Data Deduplication was enabled with a minimum file age of $minimumFileAge "
+    if ($minimumFileAge -gt 1){
+        $fo += "days."
+    }
+    else{
+        $fo += "day."
+    }
+}
+""
+Write-Host $fo -ForegroundColor "Green"
