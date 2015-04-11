953cd437006ea1efe1c929b2a1faf5e2dee04ead
Connor Kennedy <cjtkennedy@gmail.com>
2015-04-11 16:33:04 -0500

    Organization // Tiered storage space deduplication

--- CreateTieredStorageSpaceDisk.ps1
+++ CreateTieredStorageSpaceDisk.ps1
@@ -1,16 +1,17 @@
 ﻿#############################################################################
 #############################################################################
 ####                                                                     ####
-####     Creates a new tiered virtual disk in the 'SP1' storage pool     ####
-####               using the parameters specified below                  ####
+####             Creates a new tiered virtual disk using                 ####
+####                 the parameters specified below                      ####
 ####                                                                     ####
 #############################################################################
 ################################ Disk Info ##################################
 ####                                                                     ####
-<##>  $diskName = "Test1"                                                ####
-<##>  $HDD_tierSize = "5gb"                                              ####
-<##>  $SSD_tierSize = "2gb"                                              ####
-<##>  $writeCacheSize = "2gb"                                            ####
+<##>  $storagePool = "Pool1"         # Pool in which to create disk      ####
+<##>  $diskName = "Test1"            # Name of new disk                  ####
+<##>  $HDD_tierSize = "123gb"        # Size of HDD storage tier          ####
+<##>  $SSD_tierSize = "45gb"         # Size of SSD storage tier          ####
+<##>  $writeCacheSize = "67gb"       # Size of writeback cache           ####
 <##>  $resiliencySetting = "Simple"  # Simple, Mirror, or Parity         ####
 ####                                                                     ####
 #############################################################################
@@ -18,7 +19,13 @@
 ####                                                                     ####
 <##>  $partitionStyle = "GPT"        # MBR or GPT                        ####
 <##>  $fileSystem = "ReFS"           # NTFS, ReFS, exFAT, FAT32, or FAT  ####
-<##>  $partitionName = "Test1"                                           ####
+<##>  $partitionName = "Test1"       # Name of new partition             ####
+####                                                                     ####
+#############################################################################
+########################### Deduplication Info ##############################
+####                                                                     ####
+<##>  $enableDeduplication = $true   # $true or $false                   ####
+<##>  $minimumFileAge = 1            # Days                              ####
 ####                                                                     ####
 #############################################################################
 #############################################################################
@@ -34,7 +41,7 @@ $hdds = Get-StorageTier -FriendlyName Microsoft_HDD_Template
 ## TODO: Figure out why this is breaking
 ##
 # Create virtual disk
-$cmd = "New-VirtualDisk -StoragePoolFriendlyName `"SP1`" -FriendlyName $diskName -StorageTiers @($ssds,$hdds) -StorageTierSizes @($SSD_tierSize,$HDD_tierSize) -ResiliencySettingName $resiliencySetting -WriteCacheSize $writeCacheSize"
+$cmd = "New-VirtualDisk -StoragePoolFriendlyName `"$storagePool`" -FriendlyName `"$diskName`" -StorageTiers $ssds,$hdds -StorageTierSizes $SSD_tierSize,$HDD_tierSize -ResiliencySettingName $resiliencySetting -WriteCacheSize $writeCacheSize"
 
 # Run command to create virtual disk
 $cmdRes = iex $cmd
@@ -42,6 +49,10 @@ $cmdRes = iex $cmd
 "Disk `"$diskName`" was created successfully!"
 ""
 "Formatting new disk..."
+
+# Wait for new disk to mount after creation
+# This might need to be adjusted if the machine has a slow storage interface
+# There might be a better way to do this than to just wait half a second
 [System.Threading.Thread]::Sleep(500)
 
 # Get newly created disk
@@ -53,7 +64,7 @@ New-Partition -AssignDriveLetter -UseMaximumSize |
 Format-Volume -FileSystem $fileSystem -NewFileSystemLabel $partitionName -Confirm:$false
 
 # Get user-friendly size
-$labels = @("B", "KB", "MB", "GB", "TB")
+$labels = @("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
 $runs = 0
 $size = $newVolume.SizeRemaining
 $label = ""
@@ -69,4 +80,15 @@ while (1){
 }
 
 ## Display final output
-[String]::Format("The {0} partiton `"{1}`" was successfully created and mounted at {2}: with {3} of free space", $newVolume.FileSystem, $newVolume.FileSystemLabel, $newVolume.DriveLetter, $label)
+$fo = [String]::Format("The {0} partiton `"{1}`" was created successfully and mounted at {2}: with {3} of free space.{4}", $newVolume.FileSystem, $newVolume.FileSystemLabel, $newVolume.DriveLetter, $label, [Environment]::NewLine)
+if ($enableDeduplication -eq $true){
+    $fo += "Data Deduplication was enabled with a minimum file age of $minimumFileAge "
+    if ($minimumFileAge -gt 1){
+        $fo += "days."
+    }
+    else{
+        $fo += "day."
+    }
+}
+""
+Write-Host $fo -ForegroundColor "Green"
