7d9cfd764daeaef85ea89467b2cc3037a07987a3
Connor Kennedy <cjtkennedy@gmail.com>
2015-08-19 23:28:57 -0500

    Add ConsoleUtils module

---
+++ ConsoleUtils.psm1
@@ -0,0 +1,18 @@
+﻿###
+# Console utility module
+###
+
+function Prompt-Host{
+param([string]$message)
+    while($true){
+        Write-Host "$message (Y / N)?"
+        $res = Read-Host
+
+        if ($res -ieq "Y"){
+            return $true
+        }
+        elseif ($res -ieq "N"){
+            return $false
+        }
+    }
+}
