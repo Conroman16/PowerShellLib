706a4ac552c4ffbbfa1cc2759e4ff02de08d3f85
Connor Kennedy <cjtkennedy@gmail.com>
2015-08-21 11:27:37 -0500

    Update StartNodeJsCmd module

--- Modules/StartNodeJsCmd.psm1
+++ Modules/StartNodeJsCmd.psm1
@@ -1,7 +1,14 @@
 Function StartNodeJsCmd{
 param([string]$cwd, [string]$command, [bool]$waitForExit = $false)
-    $nodevarsPath = "C:\Program Files\nodejs\nodevars.bat"
-    $argStr = "/k `"`"$nodevarsPath`" & cd `"$cwd`" & $command`""
+    $nodevarsPath = "$env:ProgramFiles\nodejs\nodevars.bat"
+    $argStr = "/k `"`"$nodevarsPath`""
+    if (![string]::IsNullOrWhiteSpace($cwd)){
+        $argStr = $argStr + " & cd `"$cwd`""
+    }
+    if (![string]::IsNullOrWhiteSpace($command)){
+        $argStr = $argStr + " & $command"
+    }
+    $argStr += "`""
 
     # Initialize process
     $nodeCmdProcStartInfo = New-Object System.Diagnostics.ProcessStartInfo "cmd"
