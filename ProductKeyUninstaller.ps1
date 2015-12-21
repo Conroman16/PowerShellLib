a1b4a56179efdc7d60ab7d643dba2fbbb6315572
Connor Kennedy <cjtkennedy@gmail.com>
2015-12-21 10:30:35 -0600

    Add ProductKeyUninstaller

---
+++ ProductKeyUninstaller.ps1
@@ -0,0 +1,13 @@
+﻿function Get-ActivationID{
+    $output = cscript C:\Windows\System32\slmgr.vbs /dlv
+    $activationID = $output | Where-Object { $_.Contains("Activation ID") }
+    return $activationID.Replace("Activation ID:", "").Trim()
+}
+
+function Uninstall-ProductKey{
+    $activationID = Get-ActivationID
+    cscript C:\Windows\System32\slmgr.vbs /upk $activationID
+}
+
+# Uninstall the product key
+Uninstall-ProductKey
