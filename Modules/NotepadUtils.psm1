806a01838b953d90586b11d0b194250fab6809f5
Connor Kennedy <cjtkennedy@gmail.com>
2017-06-28 23:10:00 -0500

    Document NotepadUtils module's usage of C# and Win32

--- Modules/NotepadUtils.psm1
+++ Modules/NotepadUtils.psm1
@@ -11,27 +11,33 @@ $cSharpSignature = @"
 
     public static void Open(string text = null, string title = null)
     {
+        // Start Notepad
         Process notepad = Process.Start(new ProcessStartInfo("notepad.exe"));
-        if (notepad != null)
+        if (notepad == null || notepad.HasExited)
+            return;
+
+        notepad.WaitForInputIdle();
+
+        // Set the Notepad window title if one was specified
+        if (!string.IsNullOrEmpty(title))
+            SetWindowText(notepad.MainWindowHandle, title);
+
+        // Set the Notepad window text if specified
+        if (!string.IsNullOrEmpty(text))
         {
-            notepad.WaitForInputIdle();
+            // Get a pointer to Notepad's editor pane
+            IntPtr child = FindWindowEx(notepad.MainWindowHandle, new IntPtr(0), "Edit", null);
 
-            if (!string.IsNullOrEmpty(title))
-                SetWindowText(notepad.MainWindowHandle, title);
-
-            if (!string.IsNullOrEmpty(text))
-            {
-                IntPtr child = FindWindowEx(notepad.MainWindowHandle, new IntPtr(0), "Edit", null);
-                SendMessage(child, 0x000C, 0, text);
-            }
+            // Send that editor pane a WM_SETTEXT message with the text we want
+            SendMessage(child, 0x000C, 0, text);
         }
     }
 "@
 
-# Build the C# so we can use it
+# Build the C# so it can be used in this module
 Add-Type -Name "Helpers" -Namespace "Notepad" -MemberDefinition $cSharpSignature -UsingNamespace "System.Diagnostics"
 
 function Open-Notepad{
-param([string]$text, [string]$title)
-    [Notepad.Helpers]::Open($text, $title)
+param([string]$WindowText, [string]$WindowTitle)
+    [Notepad.Helpers]::Open($WindowText, $WindowTitle)
 }
